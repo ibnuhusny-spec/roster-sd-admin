25340f3b44b81d1ca7522dc5ba7d207d10725cee
ibnuhusny-spec <ibnuhusny@gmail.com>
2026-02-25 17:06:12 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// File ini menyimpan konfigurasi utama Firebase Anda
+// Ganti nilai di bawah ini dengan data dari Firebase Console Anda
+
+export const firebaseConfig = {
+     apiKey: "AIzaSyAu8st2VRsNFyzC8UR8TDq8821y4_TVHVk",
+  authDomain: "roster-sekolah-sd.firebaseapp.com",
+  projectId: "roster-sekolah-sd",
+  storageBucket: "roster-sekolah-sd.firebasestorage.app",
+  messagingSenderId: "819380185646",
+  appId: "1:819380185646:web:39ae854591ca40f0498dbf"
+};
