9178277ef57c417b95c2f147090da53164123dfe
ibnuhusny-spec <ibnuhusny@gmail.com>
2026-03-08 14:06:44 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,15 @@
-// File ini menyimpan konfigurasi utama Firebase Anda
-// Ganti nilai di bawah ini dengan data dari Firebase Console Anda
+// 1. Memanggil "mesin" utama Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 
-export const firebaseConfig = {
-     apiKey: "AIzaSyAu8st2VRsNFyzC8UR8TDq8821y4_TVHVk",
+// 2. Ini adalah data konfigurasi proyek roster sekolah Anda
+const firebaseConfig = {
+  apiKey: "AIzaSyAu8st2VRsNFyzC8UR8TDq8821y4_TVHVk",
   authDomain: "roster-sekolah-sd.firebaseapp.com",
   projectId: "roster-sekolah-sd",
   storageBucket: "roster-sekolah-sd.firebasestorage.app",
   messagingSenderId: "819380185646",
   appId: "1:819380185646:web:39ae854591ca40f0498dbf"
 };
+
+// 3. Menyalakan Firebase dan membagikannya (export) dengan nama 'app'
+export const app = initializeApp(firebaseConfig);
